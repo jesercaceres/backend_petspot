d678bba68bda312d6b71ec88f3467890e40d0267
AnrryPetrin <108158140+AnrryPetrin@users.noreply.github.com>
2024-05-24 19:46:51 -0300

    feat: add initial cors configuration

---
+++ src/main/java/com/petspot/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.petspot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig {
+    
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("http:/localhost/")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
