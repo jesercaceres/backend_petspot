c4ea44c6bcf4386cdcdfc8186ba8fb3fb41931b4
AnrryPetrin <108158140+AnrryPetrin@users.noreply.github.com>
2024-06-05 19:20:15 -0300

    chore: Update CORS configuration to allow requests from the petspot-frontend.vercel.app domain

--- src/main/java/com/petspot/config/CorsConfig.java
+++ src/main/java/com/petspot/config/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("https://petspot-frontend.vercel.app/");
-                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
             }
         };
     }
